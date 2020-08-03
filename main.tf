613a40412a787dec969f5e6898c645e4bf09b33d
Fedja Bosnic <fedja.bosnic@ef.com>
2020-08-03 18:01:30 +0100

    Add terraform code

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "fakery" {
+    triggers = {
+      environment = var.environment
+      region      = var.region
+      salt        = base64sha512(file("${path.module}/../.secrets/password"))
+    }
+
+    provisioner "local-exec" {
+      command = "echo \"Running on ${var.environment}/${var.region}\""
+    }
+}
